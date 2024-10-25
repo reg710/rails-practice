3a26a89d8425e45f16acc1cc8ebdf6d3ec80b180
reg710 <reg710@gmail.com>
2024-10-24 20:53:29 -0400

    Add index migration and uniqueness validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -45,4 +45,18 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lowercase" do
+    duplicate_user = @user.dup
+    mixed_case_email = "rCoN@eMaIl.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
