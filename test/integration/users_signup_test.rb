1786b9ff66a6d24c1e2f4a4a53c2f3942533bf26
reg710 <reg710@gmail.com>
2024-11-01 18:19:52 -0400

    Failed sign up path

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "Invalid sign up information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "duck",
+          password_confirmation: "dove"
+        }
+      }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+  end
+end
