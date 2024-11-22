9e6d2eec4b39b30716c4bc9c200afc15296af34b
reg710 <reg710@gmail.com>
2024-11-22 17:41:46 -0500

    Add user fixture and valid login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,13 +2,16 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
   test "Login with invalid information" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: {
       session: {
-        email: "",
-        password: ""
+        email: @user.email,
+        password: "invalid"
       }
     }
     assert_response :unprocessable_entity
@@ -17,5 +20,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     get root_path
     assert flash.empty?
+  end
+
+  test "Login with valid information" do
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: 'password'
+      }
+    }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
