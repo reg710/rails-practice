bfa098db31b3e5660483ba417c74408a0c657532
reg710 <reg710@gmail.com>
2024-11-22 18:12:24 -0500

    Refactor login tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
   end
-  test "Login with invalid information" do
+end
+
+class InvalidPasswordTest < UsersLogin
+
+  test "login path " do
     get login_path
     assert_template 'sessions/new'
+  end
+
+  test "login with valid email/invalid password" do
     post login_path, params: {
       session: {
         email: @user.email,
@@ -15,30 +22,59 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
       }
     }
     assert_not is_logged_in?
-    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
     assert_not flash.empty?
-    assert_select 'div.alert'
-
     get root_path
     assert flash.empty?
   end
+end
 
-  test "Login with valid information followed by logout" do
+class ValidLogin < UsersLogin
+
+  def setup
+    super
     post login_path, params: {
       session: {
         email: @user.email,
         password: 'password'
       }
     }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do
+    assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
+    assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+  end
+end
+
+class Logout < ValidLogin
+
+  def setup
+    super
     delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
