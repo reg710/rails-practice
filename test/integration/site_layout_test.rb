94ada6c71629682e430b9972d03db34c7142e5bb
reg710 <reg710@gmail.com>
2024-12-15 17:13:14 -0500

    Add users index page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+
+  def setup
+    @user = users(:michael)
+  end
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,5 +18,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "signup page" do
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "users layout links for logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+  end
+
+  test "no users layout links for user not logged in" do
+    get root_path
+    assert_select "a[href=?]", users_path, count: 0
   end
 end
