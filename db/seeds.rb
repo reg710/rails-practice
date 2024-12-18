530b1e67fb0283108f1f68e1f70be82dae8523bb
reg710 <reg710@gmail.com>
2024-12-18 17:43:53 -0500

    Add admin role to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # Create a main sample user.
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
 
 # Generate a bunch of additional users.
 99.times do |n|
   name = Faker::Name.name
-  email = "example-#{n}@railstutorial.org"
+  email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name: name,
     email: email,
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -51,4 +51,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should not allow admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: {
+        password: "password",
+        password_confirmation: "password",
+        admin: 1
+      }
+    }
+    assert_not @other_user.reload.admin?
+  end
 end
