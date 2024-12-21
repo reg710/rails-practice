94624d29f8d73754d3dafafe2efb017d2baa9807
reg710 <reg710@gmail.com>
2024-12-21 14:59:53 -0500

    Create activation email tests

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # This would need to be replaced with a real email if using in production
-  default from: "from@example.com"
+  default from: "user@realdomain.com"
   layout "mailer"
 end
