36a1801023a8e8a3b88f84ad19e17a53f2b13e17
reg710 <reg710@gmail.com>
2024-12-27 18:55:11 -0500

    Exercises

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -27,8 +27,8 @@ class PasswordResetsController < ApplicationController
       @user.errors.add(:password, "can't be empty")
       render 'edit', status: :unprocessable_entity
     elsif @user.update(user_params)                     # Case 4
-      reset_session
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
