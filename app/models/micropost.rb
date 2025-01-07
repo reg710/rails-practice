82e673a2435e717ab7825142b7f92173244741a8
reg710 <reg710@gmail.com>
2025-01-07 18:20:14 -0500

    Add images to posts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image # this is using the Active Storage API
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140 }
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -68,3 +68,19 @@ class MicropostSidebarTest < MicropostsInterface
     assert_match "1 micropost", response.body
   end
 end
+
+class ImageUploadTest < MicropostsInterface
+
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type=file]'
+  end
+
+  test "should be able to attach an image" do
+    content = "Micropost test"
+    image = fixture_file_upload('kitten.jpg', 'img/jpeg')
+    post microposts_path, params: { micropost: { content: content, image: image } }
+    @micropost = assigns(:micropost)
+    assert @micropost.image.attached?
+  end
+end
